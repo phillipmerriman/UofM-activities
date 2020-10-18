ab3208daacf3b444c8cd99b91b63b955b8280658
phillipmerriman <phillipmerriman@gmail.com>
2020-10-18 11:02:38 -0500

    10-17-20

---
+++ 04-05-Unsolved/Unsolved/script.js
@@ -0,0 +1,9 @@
+
+for(let i = 0; i < document.body.children.length; i++) {
+    document.querySelectorAll("img")[0].setAttribute("src", "#");
+    document.querySelectorAll("img")[0].setAttribute("alt", "a picture");
+}
+
+for(let i = 0; i < document.body.children.length; i++) {
+    document.querySelectorAll("a")[0].setAttribute("href", "#");
+}
---
+++ Unsolved/script.js
@@ -0,0 +1,10 @@
+// document.body.article.style.font-size = 50
+// document.body.children[0].style.font-color = blue
+// document.children.last(font-color: blue;)
+
+document.body.children[1].style.fontSize = "50px";
+document.body.children[1].firstElementChild.style.fontColor = "blue";
+document.body.children[1].lastElementChild.style.fontColor = "blue";
+document.body.children[1].previousElementSibling.style.backgroundColor = "black";
+
+document.body.children[0].children[1].style.textDecoration = "underline";
